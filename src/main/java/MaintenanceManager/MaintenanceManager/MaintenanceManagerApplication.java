bcb46919cf0d1c495ee536bc6aa153dfe5967738
mikalikahi <sweeney.phil@gmail.com>
2023-02-11 19:16:58 +0800

    Initial commit for maintenance task management system

---
+++ src/main/java/MaintenanceManager/MaintenanceManager/MaintenanceManagerApplication.java
@@ -0,0 +1,13 @@
+package MaintenanceManager.MaintenanceManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenanceManagerApplication.class, args);
+	}
+
+}
