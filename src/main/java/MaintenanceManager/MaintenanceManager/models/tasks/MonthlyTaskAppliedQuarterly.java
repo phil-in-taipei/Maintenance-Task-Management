f59d80c51f3cbe7b23a8a8875295f3647e8275ac
mikalikahi <sweeney.phil@gmail.com>
2023-02-22 20:50:09 +0800

    Unique constraint annotation for 3 fields on MonthlyTaskAppliedQuarterly model

--- src/main/java/MaintenanceManager/MaintenanceManager/models/tasks/MonthlyTaskAppliedQuarterly.java
+++ src/main/java/MaintenanceManager/MaintenanceManager/models/tasks/MonthlyTaskAppliedQuarterly.java
@@ -7,6 +7,8 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 @Entity
+//@Table(uniqueConstraints=
+//           @UniqueConstraint(columnNames = {"quarter", "year", "monthlyTaskScheduler"}))
 @Getter
 @Setter
 @Builder
