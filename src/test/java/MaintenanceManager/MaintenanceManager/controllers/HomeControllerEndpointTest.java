22a411b733ed84d099535d94448cced7eaacbffa
mikalikahi <sweeney.phil@gmail.com>
2023-04-09 16:56:00 +0800

    Endpoint tests of content type, status, and model attributes in home controller

--- src/test/java/MaintenanceManager/MaintenanceManager/controllers/HomeControllerEndpointTest.java
+++ src/test/java/MaintenanceManager/MaintenanceManager/controllers/HomeControllerEndpointTest.java
@@ -1,19 +1,26 @@
 package MaintenanceManager.MaintenanceManager.controllers;
 
 import MaintenanceManager.MaintenanceManager.MaintenanceManagerApplication;
+import MaintenanceManager.MaintenanceManager.models.tasks.MaintenanceTask;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import java.util.List;
+
 
 @SpringBootTest(classes = MaintenanceManagerApplication.class)
 @ActiveProfiles("test")
@@ -27,6 +34,9 @@ public class HomeControllerEndpointTest {
         mockMvc
                 .perform(get("/"))
                 //.andDo(print())
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(MockMvcResultMatchers.content()
+                        .contentType("text/html;charset=UTF-8"))
                 .andExpect(view().name("index-no-weather"));
     }
 
@@ -36,6 +46,12 @@ public class HomeControllerEndpointTest {
         mockMvc
                 .perform(get("/landing"))
                 //.andDo(print())
+                .andExpect(MockMvcResultMatchers.content()
+                        .contentType("text/html;charset=UTF-8"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(model().attributeExists("user"))
+                .andExpect(model().attributeExists("uncompletedTasks"))
+                .andExpect(model().attributeExists("dailyTasks"))
                 .andExpect(view().name("landing-no-weather"));
     }
 
